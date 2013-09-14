0bfef1ab050bfbbc936f03df149dbfe1ba771c32
iLexiconn <ilexiconn@gmail.com>
2013-09-14 14:13:00 +0200

    Complete rewrite, now you can see it ;)

--- MineDonalds/Main.java
+++ MineDonalds/Main.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.Mod;
 
 @Mod(modid = "minedonalds", name = "MineDonalds", version = "4.0")
 //Last edited by iLexiconn
-//14:11 (2:11 PM)
+//14:12 (2:12 PM)
 public class Main {
 
 }
