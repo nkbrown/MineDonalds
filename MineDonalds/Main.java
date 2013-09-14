a054fe3afd61875cb4989e6dcd3e79d3e069e148
iLexiconn <ilexiconn@gmail.com>
2013-09-14 14:23:25 +0200

    Added entityID stuff

--- MineDonalds/Main.java
+++ MineDonalds/Main.java
@@ -1,5 +1,8 @@
 package MineDonalds;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityList;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.network.NetworkMod;
@@ -7,7 +10,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = "minedonalds", name = "MineDonalds", version = "4.0")
 @NetworkMod(serverSideRequired = false, clientSideRequired = true)
 //Last edited by iLexiconn
-//14:12 (2:12 PM)
+//14:22 (2:22 PM)
 public class Main {
 	
 	/**
@@ -16,5 +19,28 @@ public class Main {
 	@SidedProxy(clientSide="MineDonalds.ClientProxy", serverSide="MineDonalds.ServerProxy")
 	public static ServerProxy proxy;
 	
+	/**
+	 * The Entity ID Registry
+	 */
 	
+	static int startEntityId = 300;
+	
+	public static int getUniqueEntityId() {
+		do {
+			startEntityId++;
+		}
+		while (EntityList.getClassFromID(startEntityId) !=null);
+	return startEntityId++;
+	}
+	public static void registerEntityEgg(Class<? extends Entity> entity, int primaryColor, int secondaryColor) {
+		int id = getUniqueEntityId();
+		EntityList.IDtoClassMapping.put(id, entity);
+		EntityList.entityEggs.put(id, new EntityEggInfo(id, primaryColor, secondaryColor));
+		
+		
+		
+	}
+
+
+
 }
