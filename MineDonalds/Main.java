66323f529417790f8577caff4709084db4852986
iLexiconn <ilexiconn@gmail.com>
2013-09-14 14:11:59 +0200

    Main class updated

--- MineDonalds/Main.java
+++ MineDonalds/Main.java
@@ -3,6 +3,8 @@ package MineDonalds;
 import cpw.mods.fml.common.Mod;
 
 @Mod(modid = "minedonalds", name = "MineDonalds", version = "4.0")
+//Last edited by iLexiconn
+//14:11 (2:11 PM)
 public class Main {
 
 }
