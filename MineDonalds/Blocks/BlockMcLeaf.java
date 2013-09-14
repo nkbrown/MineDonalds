685fb13622c91aab84e2c7e3b12c7e6934ed558b
iLexiconn <ilexiconn@gmail.com>
2013-09-14 17:44:50 +0200

    Added some blocks

---
+++ MineDonalds/Blocks/BlockMcLeaf.java
@@ -0,0 +1,24 @@
+package MineDonalds.Blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class BlockMcLeaf extends BlockMcDirtMcStone
+{
+	public BlockMcLeaf(int par1)
+	{
+		super(par1, Material.leaves);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setStepSound(soundGrassFootstep);
+		this.setHardness(0.2F);
+	}
+
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+	public void registerIcons(IconRegister iconRegister) {
+		blockIcon = iconRegister.registerIcon("minedonalds:McLeaf");
+	}
+}
--- MineDonalds/Main.java
+++ MineDonalds/Main.java
@@ -1,25 +1,22 @@
 package MineDonalds;
 
-import MineDonalds.Mobs.EntityEmployee;
-import MineDonalds.Mobs.EntityFatZombie;
-import MineDonalds.Mobs.EntityMcZombie;
-import net.minecraft.block.Block;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityEggInfo;
-import net.minecraft.entity.EntityList;
-import net.minecraft.entity.EnumCreatureType;
-import cpw.mods.fml.common.Mod;
+import MineDonalds.Blocks.*;
+import MineDonalds.Mobs.*;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.*;
+import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = "minedonalds", name = "MineDonalds", version = "4.0")
 @NetworkMod(serverSideRequired = false, clientSideRequired = true)
 //Last edited by iLexiconn
-//16:18 (4:18 PM)
+//17:44 (5:55 PM)
 public class Main {
 	
 	/**
@@ -47,6 +44,16 @@ public class Main {
 		
 		
 	}
+	
+	/**
+	 * The blocks!
+	 */
+	public static Block McGrass = new BlockMcGrass(201).setStepSound(Block.soundGrassFootstep).setHardness(0.5F).setUnlocalizedName("McGrass");
+	public static Block McDirt = new BlockMcDirtMcStone(202, Material.ground).setStepSound(Block.soundGravelFootstep).setHardness(0.5F).setUnlocalizedName("McDirt");
+	public static Block McStone = new BlockMcDirtMcStone(203, Material.rock).setHardness(2.0F).setUnlocalizedName("McStone");
+	public static Block McLeaf = new BlockMcLeaf(204).setHardness(0.2F).setUnlocalizedName("McLeaf");
+	public static Block McLog = new BlockMcLog(205).setHardness(2.0F).setUnlocalizedName("McLog");
+	public static Block McVine = new BlockMcVine(206).setHardness(0.0F).setStepSound(Block.soundGrassFootstep).setUnlocalizedName("McVine");
 	
 	@EventHandler
     public void load(FMLInitializationEvent event) {
@@ -79,6 +86,26 @@ public class Main {
     		LanguageRegistry.instance().addStringLocalization("entity.FatZombie.name", "en_US", "Fat Zombie");
     		LanguageRegistry.instance().addStringLocalization("entity.FatZombie.name", "nl_NL", "Dikke Zombie");
     		registerEntityEgg(EntityFatZombie.class, 0xFF0000, 0x096910);
+    		
+    		/**
+    		 * GameRegistry
+    		 */
+    		GameRegistry.registerBlock(McGrass, "McGrass");
+    		GameRegistry.registerBlock(McDirt, "McDirt");
+    		GameRegistry.registerBlock(McStone, "McStone");
+    		GameRegistry.registerBlock(McLeaf, "McLeaf");
+    		GameRegistry.registerBlock(McLog, "McLog");
+    		GameRegistry.registerBlock(McVine, "McVine");
+    		
+    		/**
+    		 * LanguageRegistry
+    		 */
+    		LanguageRegistry.addName(McGrass, "McGrass");
+            LanguageRegistry.addName(McDirt, "McDirt");
+            LanguageRegistry.addName(McStone, "McStone");
+            LanguageRegistry.addName(McLeaf, "McLeaf");
+            LanguageRegistry.addName(McLog, "McLog");
+            LanguageRegistry.addName(McVine, "McVine");
             
 }
 }
